fc17fda40a9f5b9551c9de88cdfe5e2076dfae1d
LuisMuriñllo93 <murillo.luis@correounivalle.edu.co>
2021-10-10 21:35:00 -0500

    se actualizan cambios

--- chispita_api/api_app/admin.py
+++ chispita_api/api_app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from .models import modeloProducto
+from .models import modeloUsuario
+from .models import modeloRol
+from .models import modeloTienda
+from .models import modeloEstado
+from .models import modeloVenta
+
 # Register your models here.
+admin.site.register(modeloProducto)
+admin.site.register(modeloUsuario)
+admin.site.register(modeloRol)
+admin.site.register(modeloTienda)
+admin.site.register(modeloEstado)
+admin.site.register(modeloVenta)
+
+
+
+
+
+
---
+++ chispita_api/api_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import viewUsuario
+
+urlpatterns = [
+    path('api_app/', viewUsuario.as_view()),
+]
+
